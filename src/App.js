24d359e7863cdd194b5930977cb68089192808dd
giri <ho1325646@gmail.com>
2021-09-16 09:40:26 +0900

    사용자 로그인 관리

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import Login from "./pages/Login";
 import ContactList from "./pages/ContactList";
 import ContactRegister from "./pages/ContactRegister";
 import NotFound from "./pages/NotFound";
+import cookie from "react-cookies";
+import { useHistory } from "react-router";
+import { useState } from "react";
+
+const getCurrentUser = () => {
+  return cookie.load("accessToken");
+};
 
 function App() {
+  const history = useHistory();
+
+  if(getCurrentUser() === undefined) {
+    history.push("/");
+  } else {
+    history.push("/list");
+  }
   return (
     <Switch>
       <Route exact path="/list" component={ContactList} />
